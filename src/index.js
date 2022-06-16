44a3d15fecf77dcff9f00abef59ca0a815c12338
Sergei Burtsev <spburtsev@gmail.com>
2022-06-17 01:24:02 +0300

    Store provider added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/auth-context';
 import { LocaleContextProvider } from './context/locale-context';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import store from './context/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -13,11 +15,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
     <LocaleContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Provider>
     </LocaleContextProvider>
   </AuthContextProvider>,
 );
