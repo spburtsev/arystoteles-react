fe3bf634cfe25aa154005e8f11b7fb3f787152bb
Sergei Burtsev <spburtsev@gmail.com>
2022-06-16 21:59:32 +0300

    Minor layout fix

--- src/components/register-page/RegisterForm.js
+++ src/components/register-page/RegisterForm.js
@@ -5,6 +5,7 @@ import useLocale from '../../hooks/use-locale';
 import useAuth from '../../hooks/use-auth';
 import { sendRegisterRequest } from '../../lib/api/user';
 import RequestStatus from '../../lib/enums/RequestStatus';
+import UserRole from '../../lib/enums/UserRole';
 import classes from './RegisterForm.module.css';
 
 const RegisterForm = () => {
@@ -12,9 +13,11 @@ const RegisterForm = () => {
   const history = useHistory();
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
+  const repeatedPasswordInputRef = useRef(null);
   const nameInputRef = useRef(null);
   const surnameInputRef = useRef(null);
   const countryInputRef = useRef(null);
+  const roleInputRef = useRef(null);
   const {
     sendRequest: sendRegister,
     status: registerStatus,
@@ -58,30 +61,60 @@ const RegisterForm = () => {
   return (
     <section className={classes.auth}>
       <h1>{locale.register}</h1>
+      <hr />
       <form onSubmit={submitHandler} className={classes.grid}>
-        <div className={classes.control}>
-          <label htmlFor="name">{locale.name}</label>
-          <input type="text" id="name" required ref={nameInputRef} />
+        <div className={classes.col}>
+          <div className={classes.control}>
+            <label htmlFor="name">{locale.name}</label>
+            <input type="text" id="name" required ref={nameInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="surname">{locale.surname}</label>
+            <input type="text" id="surname" required ref={surnameInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="country">{locale.country}</label>
+            <input type="text" id="name" required ref={countryInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="role">{locale.youAre}</label>
+            <select id="role" required ref={roleInputRef}>
+              <option value={UserRole.OrganizationAdministrator}>
+                {locale.organizationAdministrator}
+              </option>
+              <option value={UserRole.Medic}>{locale.medic}</option>
+              <option value={UserRole.Caregiver}>{locale.caregiver}</option>
+            </select>
+          </div>
         </div>
-        <div className={classes.control}>
-          <label htmlFor="surname">{locale.surname}</label>
-          <input type="text" id="surname" required ref={surnameInputRef} />
+        <div className={classes.col}>
+          <div className={classes.control}>
+            <label htmlFor="email">{locale.email}</label>
+            <input type="email" id="email" required ref={emailInputRef} />
+          </div>
+
+          <div className={classes.control}>
+            <label htmlFor="password">{locale.password}</label>
+            <input
+              type="password"
+              id="password"
+              required
+              ref={passwordInputRef}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="repeatPassword">{locale.repeatPassword}</label>
+            <input
+              type="password"
+              id="repeatPassword"
+              required
+              ref={repeatedPasswordInputRef}
+            />
+          </div>
         </div>
-        <div className={classes.control}>
-          <label htmlFor="email">{locale.email}</label>
-          <input type="email" id="email" required ref={emailInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">{locale.password}</label>
-          <input
-            type="password"
-            id="password"
-            required
-            ref={passwordInputRef}
-          />
-        </div>
+
         <div className={classes.actions}>
-          {!isLoading && <button>{locale.register}</button>}
+          {!isLoading && <button>{locale.submit}</button>}
           {isLoading && <p>{locale.sendingRequest}</p>}
         </div>
       </form>
